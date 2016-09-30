d66c55f3ba30b4fd4eb992de4809ec82b6d172ed
Sebastian Cain <sebastian.cain.stuy@gmail.com>
2016-09-30 07:51:01 -0400

    submit

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 
 def route1():
-	return render_template("index.html", title = "lmaoaml")
+    return render_template("index.html")
 
-@app.route("/two")
+@app.route("/auth", methods=['POST'])
 
 def route2():
-	return "this is the second route."
+    print request.method
+    if request.form["user"] == "tester" and request.form["pass"] == "pass":
+        return "login successful"
+    return "login unsuccessful. try again"
 
 @app.route("/three")
 
