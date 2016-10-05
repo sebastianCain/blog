52864f22fedccdf4d9035e71db2567db97810e10
Sebastian Cain <sebastian.cain.stuy@gmail.com>
2016-10-05 00:55:30 -0400

    finished assignment due wed

--- app.py
+++ app.py
@@ -8,23 +8,43 @@ app = Flask(__name__)
 def route1():
     return render_template("index.html")
 
-@app.route("/login", methods=['POST'])
+@app.route("/login", methods=['GET', 'POST'])
 
 def login():
     return render_template("login.html")
 
-@app.route("/loginauth", methods=['POST'])
+@app.route("/loginauth", methods=['GET', 'POST'])
 
 def route2():
     print request.method
-    if request.form["user"] == "tester" and request.form["pass"] == "pass":
-        return "login successful"
-    return "login unsuccessful. try again"
+    
+    occu = open("auth.csv", "r")
+    streamy = occu.read()
+    
+    data = streamy.strip().split("\n")
+    for i in data:
+        line = i.split(",")
+        user = request.form["user"]
+        hashobject = hashlib.sha256(request.form["pass"])
+        hexdig = hashobject.hexdigest()
+        if user == line[0] and hexdig == line[1]:
+            return "login successful"
+    return "login unsuccessful. <a href='/login'>try again</a>"
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 
 def route3():
-    return render_template("login.html")
+    return render_template("register.html")
+
+@app.route("/registerauth", methods=['GET', 'POST'])
+
+def route4():
+    occu = open("auth.csv", "a")
+    hashobject = hashlib.sha256(request.form["pass"])
+    hexdig = hashobject.hexdigest()
+    occu.write(request.form["user"] + "," + hexdig + "\n")
+
+    return "register successful <a href='/'>back home</a>"
 
 if __name__ == "__main__":
     app.debug = True
