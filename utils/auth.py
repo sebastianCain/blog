6d9a973100d9310b6d045e9607a563cb20ba77b4
billywongstuy <bwong5@stuy.edu>
2016-10-31 16:06:28 -0400

    added conceptuals for auth and newupdate, but no app.py to test

--- utils/auth.py
+++ utils/auth.py
@@ -1,5 +1,9 @@
 import hashlib, sqlite3
 
+f = "../data/story.db"
+db = sqlite3.connect(f)
+c = db.cursor()
+
 def hashy(word):
     myHashObject = hashlib.sha256()
     w = word.encode('utf-8')
@@ -9,17 +13,31 @@ def hashy(word):
 
 
 def authenticate(username,password):
-    f = "/data/story.db"
-    db = sqlite3.connect(f)
-    c = db.cursor()
     q = "SELECT username,password FROM users WHERE username = %s" % (username)
     a = c.execute(q)
-    if (len(a) == 0):
+    if len(a) == 0:
+        db.close()
         return False #NOT FOUND
     for user in a:
         if username == user[0]:
             if hashy(password) == user[1]:
+                db.close()
                 return True
+    db.close()
     return False
     
+
+def addUser(username,password):
+    if password == "" or username == "":
+        db.close()
+        return "Registration invalid!"
+    q = "SELECT username FROM users WHERE username = %s" % (username)
+    a = c.execute(q)
+    if len(a) > 0:
+        db.close()
+        return "User already registered!"
+    q = "INSERT INTO users VALUES (\"%s\", \"%s\")" % (username,hashy(password))
+    db.commit()
+    db.close()
+    return "User successfully registered!"
 
