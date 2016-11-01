a4cf3e437dd5fc753a873548ad5e1077ec1af6f5
Billy Wong <bwong5@stuy.edu>
2016-10-31 20:08:13 -0400

    got login and newuser working (w/out cookies)

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for, session
-import sqlite3
-import "auth/auth"
-import hashlib
+import sqlite3, hashlib, os
+from utils import auth
 
 app = Flask(__name__)
 
-app.secret_key = "mwebfailusgdfaiweflauksbdflakjbsdfiquwe"
+#app.secret_key = "mwebfailusgdfaiweflauksbdflakjbsdfiquwe"
+app.secret_key = os.urandom(32)
 
 @app.route("/")
 
 def route1():
     if "user" in session:
         return render_template("welcome.html", name= session["user"])
-    return render_template("index.html")
+    return render_template("login.html")
 
 @app.route("/logout")
 
@@ -28,30 +28,29 @@ def login():
 @app.route("/loginauth", methods=['GET', 'POST'])
 
 def route2():
-    #print request.method
-
-    line = i.split(",")
-    user = request.form["user"]
-    passwd = request.form["pass"]
     
-    if authenticate(user, passwd):
-            return "login successful <a href='/'>back home</a>"
-    return "login unsuccessful. <a href='/login'>try again</a>"
+    user = request.form["usr"]
+    passwd = request.form["pw"]
+    
+    if auth.authenticate(user, passwd):
+        return render_template("welcome.html")
+    return redirect("/")
+
 
 @app.route("/register", methods=['GET', 'POST'])
 
 def route3():
     return render_template("register.html")
 
+
 @app.route("/registerauth", methods=['GET', 'POST'])
 
 def route4():
-    occu = open("data/auth.csv", "a")
-    hashobject = hashlib.sha256(request.form["pass"])
-    hexdig = hashobject.hexdigest()
-    occu.write(request.form["user"] + "," + hexdig + "\n")
-
-    return "register successful <a href='/'>back home</a>"
+    user = request.form["user"]
+    passw = request.form["pass"]
+    if (auth.addUser(user,passw) == "User successfully registered!"):
+        return "register successful <a href='/'>back home</a>"
+    return redirect("/")
 
 if __name__ == "__main__":
     app.debug = True
--- utils/auth.py
+++ utils/auth.py
@@ -1,9 +1,5 @@
 import hashlib, sqlite3
 
-f = "../data/story.db"
-db = sqlite3.connect(f)
-c = db.cursor()
-
 def hashy(word):
     myHashObject = hashlib.sha256()
     w = word.encode('utf-8')
@@ -13,30 +9,34 @@ def hashy(word):
 
 
 def authenticate(username,password):
+    f = "data/story.db"
+    db = sqlite3.connect(f)
+    c = db.cursor()
     q = "SELECT username,password FROM users WHERE username = %s" % (username)
     a = c.execute(q)
-    if len(a) == 0:
-        db.close()
-        return False #NOT FOUND
-    for user in a:
-        if username == user[0]:
-            if hashy(password) == user[1]:
-                db.close()
-                return True
+    b = c.fetchone()
+    if b != None:
+        if b[0] == username and b[1] == hashy(password):
+            db.close()
+            return True
     db.close()
     return False
     
 
 def addUser(username,password):
+    f = "data/story.db"
+    db = sqlite3.connect(f)
+    c = db.cursor()
     if password == "" or username == "":
         db.close()
         return "Registration invalid!"
-    q = "SELECT username FROM users WHERE username = %s" % (username)
-    a = c.execute(q)
+    c.execute("SELECT username FROM users WHERE username = %s" % (username))
+    a = c.fetchall()
     if len(a) > 0:
         db.close()
         return "User already registered!"
-    q = "INSERT INTO users VALUES (\"%s\", \"%s\")" % (username,hashy(password))
+    q = "INSERT INTO users VALUES (\"%s\", \"%s\",\"\")" % (username,hashy(password))
+    c.execute(q)
     db.commit()
     db.close()
     return "User successfully registered!"
