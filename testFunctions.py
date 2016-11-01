aaefd2535994480c3bc18c7f0085d398c215314e
Billy Wong <bwong5@stuy.edu>
2016-11-01 19:41:53 -0400

    Got add new story working and removed unnecessary files

---
+++ testFunctions.py
@@ -0,0 +1,7 @@
+from utils import newStory, auth
+
+user = "123"
+name = "OH LOL"
+text = "fun fun fun fun. oh what fun"
+
+newStory.addStory(user,name,text)
--- utils/newStory.py
+++ utils/newStory.py
@@ -1,16 +1,19 @@
 from flask import session
 import sqlite3
 
-#reminder to add a name column to ispageopen
 def addStory(user,name,text):
     f = "data/story.db"
     db = sqlite3.connect(f)
     c = db.cursor()
-    q = "SELECT storyID FROM stories"
-    c.execute(q)
-    bID = c.fetchone()
-    a = c.fetchall()
+    a = c.execute("SELECT storyID FROM updates")
+    bId = 0
     for num in a:
-        if num > bID:
-            bID = num
-    q = "INSERT INTO stories VALUES (%d, \"%s\", %d, \"%s\")" % (bId, user, 0, text)
+        if num[0] > bId:
+            bId = num[0]
+    print (bId)
+    q = "INSERT INTO updates VALUES (%d, \"%s\", %d, \"%s\")" % (bId, user, 0, text)
+    print (q)
+    c.execute(q)
+    db.commit()
+    db.close()
+    return True
