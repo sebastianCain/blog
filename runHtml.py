9c5ed4d80e5c53c77c18d5fc47e1050222004c9c
Yuxuan <ychen14@stuy.edu>
2016-11-03 14:07:49 -0400

    working on home display with jinja and css

--- runHtml.py
+++ runHtml.py
@@ -3,10 +3,9 @@
 from flask import Flask, render_template
 
 runHtml=Flask(__name__)
-
 @runHtml.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", profile="http://google.com", user="User1")
 
 if __name__ == "__main__":
     runHtml.debug = True
