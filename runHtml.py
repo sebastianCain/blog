485c34686af9ed31dac9e30f8c91c2adbc66e04f
OOYUKIOO <yuxuan.chen.up@gmail.com>
2016-11-04 05:46:09 -0400

    Working on displaying story

--- runHtml.py
+++ runHtml.py
@@ -1,11 +1,18 @@
 #To test out html files
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 runHtml=Flask(__name__)
 @runHtml.route("/")
+def story():
+    return render_template("writeStory.html")
+
+@runHtml.route("/home", methods=['POST'])
 def home():
-    return render_template("home.html", profile="http://google.com", user="User1")
+    story=request.form["story"]
+    title=request.form["title"]
+    print story + ", " + title
+    return render_template("home.html",s=story, t=title)
 
 if __name__ == "__main__":
     runHtml.debug = True
