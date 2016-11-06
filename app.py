30dcb4233b21be6d9961e9fc6dc2fcc385d0f4c0
Billy Wong <bwong5@stuy.edu>
2016-11-06 11:30:25 -0500

    linked utils

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 import sqlite3, hashlib, os
-from utils import auth
+from utils import auth, newStory, addUpdate, posts
 app = Flask(__name__)
 app.secret_key = os.urandom(32)
 
