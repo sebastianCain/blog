1f4063b93d60fa3496caf26f1df67b5777f719d1
Sebastian Cain <sebastian.cain.stuy@gmail.com>
2016-11-07 07:37:24 -0500

    commented posts.py

--- utils/posts.py
+++ utils/posts.py
@@ -3,24 +3,23 @@ import sqlite3
 
 
 def getFeedData(username):
+    #connect to database
     db = sqlite3.connect("data/story.db")
     cursor = db.cursor()
     
     query = "SELECT * FROM updates "
     contributedIDs = getContributedIDs(username)
-    
-    isFirst = True
 
+    #construct query with indeterminate # of ids to exclude
+    isFirst = True
     if contributedIDs:
         query += "WHERE"
-    
     for i in contributedIDs:
         if isFirst == False:
             query += " AND"
         else:
             isFirst = False
         query += " storyID != " + str(i)
-    #print(query)
 
     cursor.execute(query)
     fetch = cursor.fetchall()
@@ -36,6 +35,8 @@ def getFeedData(username):
         for i in range(len(recentUpdates)):
             diffupdate = recentUpdates[i]
             if storyID == diffupdate[0]:
+
+                #if update is more recent, replace
                 if updateNum > diffupdate[2]:
                     recentUpdates.pop(i)
                     recentUpdates.append(update)
@@ -51,6 +52,8 @@ def getFeedData(username):
         query = "SELECT title FROM stories WHERE storyID=\"" + str(update[0]) + "\""
         cursor.execute(query)
         fetch = cursor.fetchall()
+
+        #data is array of dicts with one entry for each story
         data.append({"storyID": update[0], "title": fetch[0][0], "text": update[3]})
 
     return data
@@ -58,6 +61,7 @@ def getFeedData(username):
 
 def getContributedIDs(username):
 
+    #connect to database
     db = sqlite3.connect("data/story.db")
     cursor = db.cursor()
     
@@ -65,6 +69,7 @@ def getContributedIDs(username):
     cursor.execute(query)
     fetch = cursor.fetchall()
 
+    #extract storyID from tuple
     data = []
     for i in fetch:
         data.append(i[0])
@@ -74,6 +79,7 @@ def getContributedIDs(username):
 
 def getContributedData(username):
 
+    #connect to database
     db = sqlite3.connect("data/story.db")
     cursor = db.cursor()
     
@@ -83,7 +89,8 @@ def getContributedData(username):
     if len(contributedIDs) <= 0:
         data = []
         return data
-    
+
+    #construct query from indetermintate # of contributed ids
     isFirst = True
     for i in contributedIDs:
         if isFirst == False:
@@ -96,6 +103,7 @@ def getContributedData(username):
     cursor.execute(query)
     fetch = cursor.fetchall()
 
+    #set up empty array for each set of updates to be distributed into
     updates = {}
     for id in contributedIDs:
         updates[id] = [""]*10
@@ -105,10 +113,8 @@ def getContributedData(username):
         user = update[1]
         updateNum = update[2]
         text = update[3]
-    
-        #print("storyid: " + str(storyID))
-        #print("updateNum: " + str(updateNum))
-        #print("user: " + user)
+
+        #change text to bold if it is by the current user
         if user == username:
             updates[storyID][updateNum] = "<b>" + text + "</b>"
         else:
@@ -120,7 +126,8 @@ def getContributedData(username):
         query = "SELECT title FROM stories WHERE storyID=\"" + str(storyID) + "\""
         cursor.execute(query)
         fetch = cursor.fetchall()
-        
+
+        #join all updates into one cohesive story
         text = " ".join(updates[storyID]).strip()
         data.append({"storyID": storyID, "title": fetch[0][0], "text": text})
 
@@ -130,6 +137,7 @@ def getContributedData(username):
 
 def getStoryInfo(username, ide):
 
+    #connect to database
     ide = int(ide)
     db = sqlite3.connect("data/story.db")
     cursor = db.cursor()
@@ -140,20 +148,13 @@ def getStoryInfo(username, ide):
     updates = cursor.fetchall()
 
     text = updates[len(updates)-1][0]
-
-    canUpdate = True
-
-    for update in updates:
-        if update[1] == username:
-            canUpdate = False
         
     cursor.execute("SELECT title,isOpen from stories WHERE storyID=%d" % (ide))
     t = cursor.fetchone()
 
     title = t[0]
 
-    print ("TEXT: " + text)
-    
+    #throw info for story into one dict
     data["title"] = title
     data["text"] = text
     data["canUpdate"] = canUpdate
